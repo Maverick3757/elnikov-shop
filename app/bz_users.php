9bc19c32750760eeb057f9f181aa6468cf7c266f
Maverick3757 <yuriy@scart.com.ua>
2017-12-16 21:28:47 +0200

    laravel upload

---
+++ app/bz_users.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class bz_users extends Model
+{
+    //
+    public $timestamps = false;
+}
---
+++ laravel/app/car_brands.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class car_brands extends Model
+{
+    //
+    public $timestamps = false;
+
+
+}
