a247c19f5cc1fdb1d5be4a4d1f789615468b63e2
Maverick3757 <yuriy@scart.com.ua>
2018-01-22 13:58:27 +0200

    laravel upload2

--- app/car_brands.php
+++ app/car_brands.php
@@ -40,5 +40,17 @@ class Car_brands extends Model
     public function getModelUrlAttribute($model_name){
         return url('/'.$this->attributes['brand_name'],['model_id'=>$model_name]);
     }
+    public function translit($str) {
+        $str = (string) $str; // преобразуем в строковое значение
+        $str = strip_tags($str); // убираем HTML-теги
+        $str = str_replace(array("\n", "\r"), " ", $str); // убираем перевод каретки
+        $str = preg_replace("/\s+/", ' ', $str); // удаляем повторяющие пробелы
+        $str = trim($str); // убираем пробелы в начале и конце строки
+        $str = function_exists('mb_strtolower') ? mb_strtolower($str, 'UTF-8') : strtolower($str,'UTF-8'); // переводим строку в нижний регистр (иногда надо задать локаль)
+        $str = strtr($str, array('а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'j','з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c','ч'=>'ch','ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya','ъ'=>'','ь'=>''));
+        $str = preg_replace("/[^0-9a-z-_ ]/i", "", $str); // очищаем строку от недопустимых символов
+        $str = str_replace(" ", "-", $str); // заменяем пробелы знаком минус
+        return $str; // возвращаем результат
+    }
 
 }
